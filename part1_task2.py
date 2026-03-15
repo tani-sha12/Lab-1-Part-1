5ee080c4124836ceed917efbf8dbbdd3dbafec2c
tani-sha12 <vonatanisha123@gmail.com>
2026-03-15 13:53:32 +0800

    Update part1_task2.py

--- part1_task2.py
+++ part1_task2.py
@@ -29,9 +29,9 @@ def reconstruct_path(prev, source, target):
 
 def format_output(path, total_dist, total_energy):
     path_str = '->'.join(path)
-    print(f"Shortest path: {path_str}.")
-    print(f"Shortest distance: {total_dist}.")
-    print(f"Total energy cost: {total_energy}.")
+    print(f"Shortest path: {path_str}")
+    print(f"Shortest distance: {total_dist}")
+    print(f"Total energy cost: {total_energy}")
 
 
 # Task 2 — Uniform-Cost Search (energy-constrained)
